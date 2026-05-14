e04d702dca815d3279c32d1a084363db1e23611a
Sanjog Thapa <sanzog03@gmail.com>
2026-05-14 14:35:42 -0500

    only expose umm vis lookup tool for the fast mcp cloud module resolution to be faster. this change is for mcp preview only, remove it later before merge

--- akd_ext/tools/__init__.py
+++ akd_ext/tools/__init__.py
@@ -1,25 +1,25 @@
 """Tools module for akd_ext."""
 
-from .dummy import DummyInputSchema, DummyOutputSchema, DummyTool
-from .sde_search import (
-    SDEDocument,
-    SDESearchTool,
-    SDESearchToolConfig,
-    SDESearchToolInputSchema,
-    SDESearchToolOutputSchema,
-)
-from .code_search.code_signals import (
-    CodeSignalsSearchInputSchema,
-    CodeSignalsSearchOutputSchema,
-    CodeSignalsSearchTool,
-    CodeSignalsSearchToolConfig,
-)
-from .code_search.repository_search import (
-    RepositorySearchTool,
-    RepositorySearchToolInputSchema,
-    RepositorySearchToolOutputSchema,
-    RepositorySearchToolConfig,
-)
+# from .dummy import DummyInputSchema, DummyOutputSchema, DummyTool
+# from .sde_search import (
+#     SDEDocument,
+#     SDESearchTool,
+#     SDESearchToolConfig,
+#     SDESearchToolInputSchema,
+#     SDESearchToolOutputSchema,
+# )
+# from .code_search.code_signals import (
+#     CodeSignalsSearchInputSchema,
+#     CodeSignalsSearchOutputSchema,
+#     CodeSignalsSearchTool,
+#     CodeSignalsSearchToolConfig,
+# )
+# from .code_search.repository_search import (
+#     RepositorySearchTool,
+#     RepositorySearchToolInputSchema,
+#     RepositorySearchToolOutputSchema,
+#     RepositorySearchToolConfig,
+# )
 
 from .worldview import (
     LayerMapping,
@@ -30,22 +30,22 @@ from .worldview import (
 )
 
 __all__ = [
-    "DummyTool",
-    "DummyInputSchema",
-    "DummyOutputSchema",
-    "SDESearchTool",
-    "SDESearchToolInputSchema",
-    "SDESearchToolOutputSchema",
-    "SDESearchToolConfig",
-    "SDEDocument",
-    "CodeSignalsSearchInputSchema",
-    "CodeSignalsSearchOutputSchema",
-    "CodeSignalsSearchTool",
-    "CodeSignalsSearchToolConfig",
-    "RepositorySearchTool",
-    "RepositorySearchToolInputSchema",
-    "RepositorySearchToolOutputSchema",
-    "RepositorySearchToolConfig",
+    # "DummyTool",
+    # "DummyInputSchema",
+    # "DummyOutputSchema",
+    # "SDESearchTool",
+    # "SDESearchToolInputSchema",
+    # "SDESearchToolOutputSchema",
+    # "SDESearchToolConfig",
+    # "SDEDocument",
+    # "CodeSignalsSearchInputSchema",
+    # "CodeSignalsSearchOutputSchema",
+    # "CodeSignalsSearchTool",
+    # "CodeSignalsSearchToolConfig",
+    # "RepositorySearchTool",
+    # "RepositorySearchToolInputSchema",
+    # "RepositorySearchToolOutputSchema",
+    # "RepositorySearchToolConfig",
     "LayerMapping",
     "UMMVisLookupTool",
     "UMMVisLookupToolConfig",
